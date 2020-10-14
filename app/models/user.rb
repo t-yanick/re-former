530f8ba137e12680dfa74a0d7425fd7026f5c25a
Yanick <tyankee15@gmail.com>
2020-10-14 16:23:15 -0300

    Add validations for presence to each field in the model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { within: 4..20 }
+  validates :email, presence: true, length: { within: 6..20 }
+  validates :username, presence: true
 end

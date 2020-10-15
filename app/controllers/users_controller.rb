789a24574dc87122916bea9eb816f14e10feecc6
Yanick <tyankee15@gmail.com>
2020-10-15 19:22:30 -0300

    TSE recommendation to use render :edit if user is not saved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,11 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to new_user_path
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :edit
+    end
   end
 
   private

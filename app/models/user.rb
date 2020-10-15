4aef131429f71796fa37a7396495a09522199af8
Yanick <tyankee15@gmail.com>
2020-10-15 09:25:38 -0300

    Refactor view file and users_controllers to get simple HTML form working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { within: 4..20 }
-  validates :email, presence: true, length: { within: 6..20 }
-  validates :username, presence: true
+  validates :password, presence: true, length: { within: 6..20 }
+  validates :email, presence: true
 end

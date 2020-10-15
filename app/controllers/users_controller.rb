b8044dcaa912d7174ccf3989aa85ae5af709e7e1
Yanick <tyankee15@gmail.com>
2020-10-15 10:24:27 -0300

    Create Edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,12 @@ end
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to new_user_path
+  end
+
   private
 
   def user_params
